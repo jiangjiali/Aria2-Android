d2c6e9185f259e22f6f6f476a848ea1d16b0e78a
蒋佳李 <jiangjiali@me.com>
2024-07-16 13:50:28 +0800

    更新

--- aria2lib/build_aria2c.sh
+++ aria2lib/build_aria2c.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 ## Clone
-git clone https://github.com/devgianlu/aria2-android --recurse-submodules --depth 1
-cd aria2-android
+git clone https://github.com/devgianlu/aria2c-android --recurse-submodules --depth 1
+cd aria2c-android
 git checkout $@
 
 ## Prepare env
